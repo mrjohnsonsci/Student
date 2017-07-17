e50a47f62c9aa072416cdf334e586b152d98bc18
Peter Johnson <pjohnson141@gmail.com>
2017-07-17 01:48:14 +0000

    finishstatic pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
+ def setup
+    @base_title = "Peter Johnson's Classroom Tools"
+ end
+  
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    #assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get contact" do
