ef241e38e17c2aa75863ccdd0efcdb0888c0d634
Peter Johnson <pjohnson141@gmail.com>
2017-07-17 16:42:02 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Peter Johnsons Classroom Tools"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
